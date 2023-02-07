3c6a1d8a000cc4850ff3bb8db7a6a439144c425d
Enzo1603 <96661578+Enzo1603@users.noreply.github.com>
2023-02-07 10:55:18 +0100

    Attach Postgresql Database

--- migrations/20230206210143_create_users.sql
+++ migrations/20230206210143_create_users.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS users
 (
-  uuid UUID PRIMARY KEY,
-  name VARCHAR NOT NULL,
-  age SMALLINT NOT NULL DEFAULT 0,
-  grade SMALLINT NOT NULL DEFAULT 0,
-  active BOOL NOT NULL DEFAULT TRUE
+    uuid            UUID PRIMARY KEY,
+    username        VARCHAR NOT NULL UNIQUE,
+    email           VARCHAR NOT NULL UNIQUE,
+    password_hash   VARCHAR NOT NULL,
+    description     TEXT,
+    status          INTEGER NOT NULL DEFAULT 0,
+    created_at      TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE INDEX name_active_idx ON users(name, active);
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
 #[macro_use]
 extern crate rocket;
 
+use chrono::{DateTime, Utc};
 use rocket::{Build, Rocket};
+use rocket_db_pools::{{sqlx::PgPool}, Database};
+use sqlx::FromRow;
+use uuid::Uuid;
+
+#[derive(sqlx::Type, Debug)]
+#[repr(i32)]
+enum UserStatus {
+    Inactive = 0,
+    Active = 1,
+}
+
+#[derive(Debug, FromRow)]
+struct User {
+    uuid: Uuid,
+    username: String,
+    email: String,
+    password_hash: String,
+    description: String,
+    status: UserStatus,
+    created_at: DateTime<Utc>,
+    updated_at: DateTime<Utc>,
+}
+
+#[derive(Database)]
+#[database("main_connection")]
+struct DBCOnnection(PgPool);
 
 #[launch]
 async fn rocket() -> Rocket<Build> {
-    rocket::build()
+    rocket::build().attach(DBCOnnection::init())
 }
